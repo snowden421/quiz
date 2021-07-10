88397f90374605b8f952f1e2c83df60c603b8eeb
Snow <84701509+snowden421@users.noreply.github.com>
2021-07-10 11:04:56 -0700

    added questions.js, question array, getQuestion function

---
+++ assets/js/questions.js
@@ -0,0 +1,11 @@
+var questions = [
+    {title: "question1", choices: ["choice1", "choice2", "choice3", "choice4"], answer: "choice1" },
+    {title: "question2", choices: ["choice1", "choice2", "choice3", "choice4"], answer: "choice1" },
+    {title: "question3", choices: ["choice1", "choice2", "choice3", "choice4"], answer: "choice1" },
+    {title: "question4", choices: ["choice1", "choice2", "choice3", "choice4"], answer: "choice1" },
+    {title: "question5", choices: ["choice1", "choice2", "choice3", "choice4"], answer: "choice1" }
+];
+
+
+
+console.log(questions[questionNumber].choices);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
-//Selectors
+// Selectors
 var startBtn = document.querySelector("#start");
 var timerEl = document.querySelector("#timer");
 var startEl = document.querySelector("#start-menu");
 var quizEl = document.querySelector("#quiz-menu");
 var scoreEl = document.querySelector("#score-menu");
-//variables
-var time = 20;
-var timeInterval;
 
-//this function starts the quiz
+// Variables
+var time = 10;
+var timeInterval;
+var questionNumber = 0
+
+
+// This function starts the quiz
 function start() {
     timeInterval = setInterval(function() {
         time--;
@@ -19,13 +22,30 @@ function start() {
     }, 1000);
     startEl.setAttribute("class", "hide");
     quizEl.removeAttribute("class");
+    // call function that puts questions and choices on screen
+};
+
+// player makes choice and clicks button, create next question function
+/*
+if (questionNumber === questions.length) {
+    // then run endgame function
+} else {
+    // then run getQuestion function
 }
+// display if choice was right or wrong
+*/
+
+// make function that puts question and choices on screen
+function getQuestion() {
+    
+}
+
 
 function endgame() {
     clearInterval(timeInterval);
     quizEl.setAttribute("class", "hide");
     scoreEl.removeAttribute("class");
-}
+};
 
 //Event Listeners
 startBtn.addEventListener("click", start);
