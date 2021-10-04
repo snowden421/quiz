dcc151adeead9fa6b80f921e15c0e4865fe02966
Snow <84701509+snowden421@users.noreply.github.com>
2021-10-04 16:00:39 -0700

    added style

---
+++ assets/js/highscore.js
@@ -0,0 +1,3 @@
+var getHighScore = function() {
+    localStorage.getItem()
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 // Selectors
 var startBtn = document.querySelector("#start");
+var submitBtn = document.querySelector("#submit-score");
 var timerEl = document.querySelector("#timer");
 var startEl = document.querySelector("#start-menu");
 var quizEl = document.querySelector("#quiz-menu");
 var scoreEl = document.querySelector("#score-menu");
+var finalScore = document.querySelector("#final-score");
+var initials = document.querySelector("#initials");
 var questionEl = document.querySelector("#question");
 var validation = document.querySelector("#validation");
 var choiceA = document.querySelector("#A");
@@ -13,6 +16,7 @@ var choiceD = document.querySelector("#D");
 
 // Variables
 var time = 0;
+var score = 0;
 var timeInterval;
 var questionNumber = 0;
 var currentQuestion = 0;
@@ -20,30 +24,44 @@ var currentQuestion = 0;
 // Questions
 var questions = [
     {title: "Arrays in JavaScript can be used to store _______",
-    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-    answer: "all of the above" },
+    choiceA: "numbers and strings",
+    choiceB: "other arrays",
+    choiceC: "booleans",
+    choiceD: "all of the above",
+    answer: "D" },
 
     {title: "Commonly used data types do NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts" },
+    choiceA: "strings",
+    choiceB: "booleans",
+    choiceC: "alerts",
+    choiceD: "numbers",
+    answer: "C" },
     
     {title: "The condition in an if/then statement is enclosed with _____",
-    choices: ["quotes", "curly brackets", "square brackets", "parenthesis"],
-    answer: "parenthesis" },
+    choiceA: "quotes",
+    choiceB: "curly brackets",
+    choiceC: "square brackets",
+    choiceD: "parenthesis",
+    answer: "D" },
     
     {title: "String values must be enclosed within ______ when being assigned to variables",
-    choices: ["commas", "curly brackets", "quotes", "parenthesis"],
-    answer: "quotes" },
+    choiceA: "commas",
+    choiceB: "curly brackets",
+    choiceC: "quotes",
+    choiceD: "parenthesis",
+    answer: "C" },
     
     {title: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choices: ["JavaScript", "terminal/Bash", "for loop", "console.log"],
-    answer: "console.log" }
+    choiceA: "JavaScript",
+    choiceB: "terminal/Bash",
+    choiceC: "for loop",
+    choiceD: "console.log",
+    answer: "D" }
 ];
 
 // This function starts the quiz
 function start() {
-    time = 10;
-    score = 0;
+    time = 60;
     currentQuestion = 0;
     timeInterval = setInterval(function() {
         time--;
@@ -83,23 +101,37 @@ function getQuestion(questionNumber) {
 }
 
 function checkAnswer(choicePicked) {
-    if (choicePicked === questions[currentQuestion].correct) {
-        score++;
+    if (choicePicked === questions[currentQuestion].answer) {
         validation.innerHTML = "Correct";
+        score++;
     }
     else {
         validation.innerHTML = "WRONG!";
-        score--;
     }
     currentQuestion++;
     getQuestion(currentQuestion);
-}
+};
 
 function endgame() {
     clearInterval(timeInterval);
     quizEl.setAttribute("class", "hide");
     scoreEl.removeAttribute("class");
+    finalScore.textContent = score;
+};
+
+function addHighScore() {
+    var init = initials.value
+    var highScores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+    var newScore = {
+        init,
+        score
+    }
+    highScores.push(newScore)
+    localStorage.setItem("highscores", JSON.stringify(highScores));
+
+    window.location.reload();
 };
 
 //Event Listeners
 startBtn.addEventListener("click", start);
+submitBtn.addEventListener("click", addHighScore);
